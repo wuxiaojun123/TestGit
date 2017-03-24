705bfa78ca7f0946eafb3490ddd56358936d8dd3
wuxiaojun1994 <549661646@qq.com>
2017-03-24 23:29:49 +0800

    f1分支提交

---
+++ TestProject/app/src/main/java/com/wxj/test/App.java
@@ -0,0 +1,16 @@
+package com.wxj.test;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wuxiaojun on 2017/3/24.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
